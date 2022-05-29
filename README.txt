e01598643efa290c6e15df16fbaa55329e2c8317
shiftertioio <82117705+shiftertioio@users.noreply.github.com>
2022-05-30 00:20:24 +0800

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,15 @@
+How to play the Hangman game?
+
+Player 2 will first set a word that they want Player 1 to guess.
+Player 2 will then press the "Enter Word" button to signify that Player 1 can start guessing.
+
+If Player 1 gets a wrong letter, Player 2 will press the "Wrong Letter" button.
+The hangman will advance by a stage. There are 6 stages in total.
+
+Repeat the above till the player guesses the word, in which, press the green "Word Guessed" button.
+If player fails to guess the word before the GG hangman stage, they loose the game.
+
+Click the yellow "Reveal Word" button to reveal the word hidden.
+Press the black "End game" button to restart the game.
+
+End of instructions
---
+++ js/app.js
@@ -0,0 +1,28 @@
+function wawa() {
+  var variable = document.getElementById('input_id').value;
+  document.getElementById('alert').innerHTML = 'The user input is: ' + variable;
+}
+
+$(document).ready(function() {
+    $("#show_hide_password a").on('click', function(event) {
+        event.preventDefault();
+        if($('#show_hide_password input').attr("type") == "text"){
+            $('#show_hide_password input').attr('type', 'password');
+            $('#show_hide_password i').addClass( "fa-eye-slash" );
+            $('#show_hide_password i').removeClass( "fa-eye" );
+        }else if($('#show_hide_password input').attr("type") == "password"){
+            $('#show_hide_password input').attr('type', 'text');
+            $('#show_hide_password i').removeClass( "fa-eye-slash" );
+            $('#show_hide_password i').addClass( "fa-eye" );
+        }
+    });
+});
+
+ function myFunction() {
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
